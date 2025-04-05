8d6622eb68b363317aa45d60a6e01d6b70027954
Aakash J John <127650255+AakashJai06@users.noreply.github.com>
2025-04-05 19:06:19 +0530

    Update server.c

--- File Transfer Protocol/server.c
+++ File Transfer Protocol/server.c
@@ -1,43 +1,50 @@
-#include<stdio.h>
-#include<arpa/inet.h>
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include<netdb.h>
-#include<stdlib.h>
-#include<string.h>
-#include<unistd.h>
-#define SERV_TCP_PORT 5035
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 5035
 #define MAX 60
-int i, j, tem;
-char buff[4096], t;
-FILE *f1;
-int main(int afg, char *argv)
-{
-       int sockfd, newsockfd, clength;
-       struct sockaddr_in serv_addr,cli_addr;
-       char t[MAX], str[MAX];
-       strcpy(t,"exit");
-       sockfd=socket(AF_INET, SOCK_STREAM,0);
-       serv_addr.sin_family=AF_INET;
-       serv_addr.sin_addr.s_addr=INADDR_ANY;
-       serv_addr.sin_port=htons(SERV_TCP_PORT);
-       printf("\nBinded");
-       bind(sockfd,(struct sockaddr*)&serv_addr, sizeof(serv_addr));
-       printf("\nListening...");
-       listen(sockfd, 5);
-       clength=sizeof(cli_addr);
-       newsockfd=accept(sockfd,(struct sockaddr*) &cli_addr,&clength);
-       close(sockfd);
-       read(newsockfd, &str, MAX);
-       printf("\nClient message\n File Name : %s\n", str);
-       f1=fopen(str, "r");
-       while(fgets(buff, 4096, f1)!=NULL)
-       {
-            write(newsockfd, buff,MAX);
-            printf("\n");
-       }
-       fclose(f1);
-       printf("\nFile Transferred\n");
-       return 0;
+
+int main() {
+    int sockfd, newsockfd;
+    struct sockaddr_in serv_addr, cli_addr;
+    socklen_t clilen;
+    char filename[MAX], buffer[4096];
+    FILE *file;
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(PORT);
+
+    bind(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+    listen(sockfd, 5);
+
+    printf("Server listening on port %d...\n", PORT);
+    clilen = sizeof(cli_addr);
+    newsockfd = accept(sockfd, (struct sockaddr*)&cli_addr, &clilen);
+
+    read(newsockfd, filename, MAX);
+    printf("Requested file: %s\n", filename);
+
+    file = fopen(filename, "r");
+    if (!file) {
+        perror("File open failed");
+        close(newsockfd);
+        close(sockfd);
+        return 1;
+    }
+
+    while (fgets(buffer, sizeof(buffer), file) != NULL) {
+        write(newsockfd, buffer, MAX);
+    }
+
+    fclose(file);
+    close(newsockfd);
+    close(sockfd);
+    printf("File sent successfully.\n");
+    return 0;
 }
